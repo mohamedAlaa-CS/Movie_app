384c24b5d1cb62cc88bac24b54121f6b104f9505
Myproject <mohamed.alaa@gmail.com>
2023-11-20 23:32:18 +0200

    splash feature

--- lib/features/splash/presentaion/view/spalsh_view.dart
+++ lib/features/splash/presentaion/view/spalsh_view.dart
@@ -1,10 +1,32 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:movie/core/utils/assets.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {});
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage(
+            Assets.logo,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Movie App',
       theme: appTheme,
       home: const SplashView(),
