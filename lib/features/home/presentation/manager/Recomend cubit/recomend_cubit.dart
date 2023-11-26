ae8bb3019d2ba1f721b62d8651e027d970f2e588
Myproject <mohamed.alaa@gmail.com>
2023-11-26 19:25:32 +0200

    recomend cubit

--- lib/features/home/presentation/manager/Recomend cubit/recomend_cubit.dart
+++ lib/features/home/presentation/manager/Recomend cubit/recomend_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie/features/home/data/model/recomend_model.dart';
+import 'package:movie/features/home/data/repo/home_repo.dart';
 
 part 'recomend_state.dart';
 
 class RecomendCubit extends Cubit<RecomendState> {
-  RecomendCubit() : super(RecomendInitial());
+  RecomendCubit(this.homeRepo) : super(RecomendInitial());
+  final HomeRepo homeRepo;
+  featchRecomendMovie() async {
+    emit(RecomendLoading());
+    var result = await homeRepo.feactchRecomendMovie();
+    result.fold((failuer) {
+      emit(RecomendFailure(failuer.errorMessage));
+    }, (recomendList) {
+      emit(RecomendSuccess(recomendList));
+    });
+  }
 }
