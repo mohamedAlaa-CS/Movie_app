0da3b3d84e015b05a90160b53c2562b5ce64ead8
Myproject <mohamed.alaa@gmail.com>
2023-11-30 16:07:10 +0200

    featch details method

--- lib/features/home/presentation/manager/Details cubit/details_cubit.dart
+++ lib/features/home/presentation/manager/Details cubit/details_cubit.dart
@@ -1,8 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:movie/features/home/data/model/details/details.model.dart';
+import 'package:movie/features/home/data/repos/home%20details%20repo/home_details_repo.dart';
 
 part 'details_state.dart';
 
 class DetailsCubit extends Cubit<DetailsState> {
-  DetailsCubit() : super(DetailsInitial());
+  final HomeDetailsRepo homeDetailsRepo;
+  DetailsCubit(this.homeDetailsRepo) : super(DetailsInitial());
+
+  //! featch details
+  late DetailsModel detailsModel;
+  featchDetails(query) async {
+    emit(DetailsLoading());
+    var result = await homeDetailsRepo.featchDetails(query: query);
+    result.fold((failuer) {
+      emit(DetailsFailuer(failuer.errorMessage));
+    }, (details) {
+      detailsModel = details;
+      emit(DetailsSuccess());
+    });
+  }
 }
